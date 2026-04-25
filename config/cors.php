a3de7b698712be4dbce09227c261bd3534bf69ff
dynozstore-cell <dynoz.store@gmail.com>
2026-04-25 12:34:41 +0700

    fix: remove artisan calls (breaking cors)

--- config/cors.php
+++ config/cors.php
@@ -4,9 +4,17 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
+    'allowed_origins' => [
+        'https://kesavent.vercel.app',
+    ],
+
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
 
     'supports_credentials' => false,
 
