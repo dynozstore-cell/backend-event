03483f3b485ef89d6e606e96b50bf63907d134e3
dynozstore-cell <dynoz.store@gmail.com>
2026-04-25 12:16:07 +0700

    fix: cors allow all

--- config/cors.php
+++ config/cors.php
@@ -4,17 +4,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://kesavent.vercel.app',
-    ],
-
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => ['*'], // sementara buka dulu
 
     'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
 
     'supports_credentials' => false,
 
