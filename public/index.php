0d53bba2000b96d9099b6223f8d2f255c87b06e1
dynozstore-cell <dynoz.store@gmail.com>
2026-04-25 15:36:53 +0700

    perf: implement ultra-fast options bypass

--- public/index.php
+++ public/index.php
@@ -3,6 +3,15 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-Auth-Token');
+    header('Access-Control-Max-Age: 7200');
+    header('HTTP/1.1 204 No Content');
+    exit;
+}
+
 define('LARAVEL_START', microtime(true));
 
 /*
