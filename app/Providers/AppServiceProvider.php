68e9ef43979cca2c672a2ebda8c0113768eff153
dynozstore-cell <dynoz.store@gmail.com>
2026-04-25 14:54:43 +0700

    debug: add slow query logging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,5 +16,15 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
+
+        // Catat semua query yang lambat ke Log
+        \Illuminate\Support\Facades\DB::listen(function ($query) {
+            if ($query->time > 1000) { // Jika lebih dari 1 detik
+                \Illuminate\Support\Facades\Log::warning("Slow Query detected: " . $query->sql, [
+                    'time' => $query->time,
+                    'bindings' => $query->bindings
+                ]);
+            }
+        });
     }
 }
